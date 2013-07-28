621c92ed2b8fc8fb581aeb7e5d589de40a28ffd8
EC2 Default User <ec2-user@ip-10-40-65-192.ec2.internal>
2013-07-28 22:34:57 +0000

    tests working, some todo's todo like time expiration

--- lib/fcf.ex
+++ lib/fcf.ex
@@ -1,2 +1,38 @@
-defmodule Fcf do
+defmodule FCF do
+  def run(module,fun,args,options // []) do
+    IO.puts "Options: #{inspect options}"
+    hash = :erlang.phash2("#{inspect module}#{inspect fun}#{inspect args}",4294967296)
+    case has(hash) do
+      :false -> 
+        IO.puts "no cache, getting data: #{inspect hash}"
+        res = apply(module,fun,args)
+        store(res,hash,options)
+      res when options != [] ->   
+        IO.puts "forced cache flush, getting data: #{inspect hash}"
+        res = apply(module,fun,args)
+        store(res,hash,options)
+      res -> 
+        IO.puts "found result in cache: #{inspect hash}"
+        res
+    end
+    res
+  end
+  def flush_all do
+ 		:os.cmd('rm -rf cache/*') 
+  end
+  def has(hash) do
+    file_name = "cache/#{hash}"
+    case File.exists?(file_name) do
+      true -> 
+        {:ok,res} = File.read(file_name)
+        :erlang.binary_to_term(res)
+      false -> :false
+    end
+  end
+  def store(res,hash,options) do
+    res = :erlang.term_to_binary(res)
+    IO.puts "FCF store #{inspect res} #{inspect hash} "
+    file_name = "cache/#{hash}"
+    File.write(file_name,res)
+  end
 end
--- test/fcf_test.exs
+++ test/fcf_test.exs
@@ -1,9 +1,38 @@
 Code.require_file "test_helper.exs", __DIR__
-
+defmodule Tst do
+  def boink do
+    {1,2,3}
+  end
+  def wait do
+ 		:timer.sleep(200) 
+  end
+end
 defmodule FcfTest do
   use ExUnit.Case
-
-  test "the truth" do
-    assert(true)
+  setup_all do
+    FCF.flush_all
+    :ok
+  end
+  teardown_all do
+    FCF.flush_all
+    :ok
+  end
+  test "writes to a file" do
+    res = FCF.run(Tst,:boink,[]) 
+    assert(res = {1,2,3})
+  end
+  test "bypasses cache when it should" do
+  	t1 = :erlang.now
+ 		FCF.run(Tst,:wait,[])
+ 		t2 = :erlang.now
+ 		assert(:timer.now_diff(t2,t1) >= 200000)
+ 		t1 = :erlang.now
+ 		FCF.run(Tst,:wait,[])
+ 		t2 = :erlang.now
+ 		assert(:timer.now_diff(t2,t1) < 200000)
+ 		t1 = :erlang.now
+ 		FCF.run(Tst,:wait,[],[force: true])
+ 		t2 = :erlang.now
+ 		assert(:timer.now_diff(t2,t1) >= 200000)
   end
 end
